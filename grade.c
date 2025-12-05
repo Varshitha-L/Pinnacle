127b9061bd37d711dbef69de4ad626423e605766
Varshitha-L <varshithalvijaya@gmail.com>
2025-12-05 21:33:46 +0530

    Add program to calculate average marks

---
+++ grade.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    int n; printf("Enter number of subjects: "); scanf("%d",&n);
+    float marks[n], sum=0;
+    for(int i=0;i<n;i++){ printf("Enter mark %d: ",i+1); scanf("%f",&marks[i]); sum+=marks[i]; }
+    printf("Average = %.2f\n", sum/n);
+}
