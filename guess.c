99f68baed19c30c1d27ba836ca90ce2149249b7d
Varshitha-L <varshithalvijaya@gmail.com>
2025-12-08 22:03:02 +0530

    Create guess.c

---
+++ guess.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main() {
+    srand(time(0));
+    int num=rand()%100+1, guess;
+    do {
+        printf("Guess (1-100): ");
+        scanf("%d",&guess);
+        if(guess<num) printf("Too low!\n");
+        else if(guess>num) printf("Too high!\n");
+        else printf("Correct!\n");
+    } while(guess!=num);
+}
