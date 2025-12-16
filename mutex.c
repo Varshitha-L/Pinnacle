ca941ec6743c4b9047ab87cbd8bd5de25b1a7ac7
Varshitha-L <varshithalvijaya@gmail.com>
2025-12-16 21:07:09 +0530

    Update mutex.c

--- mutex.c
+++ mutex.c
@@ -1,58 +1,91 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #define MAX 5
-int buffer[MAX], count = 0;
+#define NUM_PRODUCERS 2
+#define NUM_CONSUMERS 2
+
+int buffer[MAX];
+int in = 0, out = 0, count = 0;
+
 pthread_mutex_t lock;
 pthread_cond_t not_full, not_empty;
 
 void* producer(void* arg) {
+    int id = *(int*)arg;
     int item = 1;
+
     while (1) {
         pthread_mutex_lock(&lock);
 
         while (count == MAX)
             pthread_cond_wait(&not_full, &lock);
 
-        buffer[count++] = item;
-        printf("Produced: %d | Count = %d\n", item, count);
+        buffer[in] = item;
+        printf("Producer %d → Produced %d at index %d | Count = %d\n",
+               id, item, in, count + 1);
+
+        in = (in + 1) % MAX;
+        count++;
         item++;
 
         pthread_cond_signal(&not_empty);
         pthread_mutex_unlock(&lock);
-        sleep(1);
+
+        usleep((rand() % 500 + 200) * 1000);
     }
 }
 
 void* consumer(void* arg) {
+    int id = *(int*)arg;
+
     while (1) {
         pthread_mutex_lock(&lock);
 
         while (count == 0)
             pthread_cond_wait(&not_empty, &lock);
 
-        int item = buffer[--count];
-        printf("Consumed: %d | Count = %d\n", item, count);
+        int item = buffer[out];
+        printf("Consumer %d → Consumed %d from index %d | Count = %d\n",
+               id, item, out, count - 1);
+
+        out = (out + 1) % MAX;
+        count--;
 
         pthread_cond_signal(&not_full);
         pthread_mutex_unlock(&lock);
-        sleep(2);
+
+        usleep((rand() % 700 + 300) * 1000);
     }
 }
 
 int main() {
-    pthread_t p, c;
+    srand(time(NULL));
+
+    pthread_t prod[NUM_PRODUCERS], cons[NUM_CONSUMERS];
+    int prod_id[NUM_PRODUCERS], cons_id[NUM_CONSUMERS];
 
     pthread_mutex_init(&lock, NULL);
     pthread_cond_init(&not_full, NULL);
     pthread_cond_init(&not_empty, NULL);
 
-    pthread_create(&p, NULL, producer, NULL);
-    pthread_create(&c, NULL, consumer, NULL);
+    for (int i = 0; i < NUM_PRODUCERS; i++) {
+        prod_id[i] = i + 1;
+        pthread_create(&prod[i], NULL, producer, &prod_id[i]);
+    }
 
-    pthread_join(p, NULL);
-    pthread_join(c, NULL);
+    for (int i = 0; i < NUM_CONSUMERS; i++) {
+        cons_id[i] = i + 1;
+        pthread_create(&cons[i], NULL, consumer, &cons_id[i]);
+    }
+
+    for (int i = 0; i < NUM_PRODUCERS; i++)
+        pthread_join(prod[i], NULL);
+
+    for (int i = 0; i < NUM_CONSUMERS; i++)
+        pthread_join(cons[i], NULL);
 
     return 0;
 }
