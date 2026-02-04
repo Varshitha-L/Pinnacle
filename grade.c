f7a434c80780d68193188a8112a08c042fbf73d7
Varshitha-L <varshithalvijaya@gmail.com>
2026-02-04 20:18:32 +0530

    Update grade.c

--- grade.c
+++ grade.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main() {
-    int n; printf("Enter number of subjects: "); scanf("%d",&n);
+    int n;
+    printf("Enter number of subjects: "); scanf("%d",&n);
     float marks[n], sum=0;
     for(int i=0;i<n;i++){ printf("Enter mark %d: ",i+1);
                          scanf("%f",&marks[i]); sum+=marks[i]; }
