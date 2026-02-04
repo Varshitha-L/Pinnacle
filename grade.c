bcc2690bdc344c7d25ac2e789e489409b47a1f18
Varshitha-L <varshithalvijaya@gmail.com>
2026-02-04 20:18:46 +0530

    Update grade.c

--- grade.c
+++ grade.c
@@ -3,7 +3,8 @@ int main() {
     int n;
     printf("Enter number of subjects: "); scanf("%d",&n);
     float marks[n], sum=0;
-    for(int i=0;i<n;i++){ printf("Enter mark %d: ",i+1);
+    for(int i=0;i<n;i++){ 
+        printf("Enter mark %d: ",i+1);
                          scanf("%f",&marks[i]); sum+=marks[i]; }
     printf("Average = %.2f\n", sum/n);
 }
